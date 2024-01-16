aa982869aa0d3ea41ed45d3ea9d32fc91ecd338b
gilsonlevi <gilsonlevi328@gmail.com>
2024-01-15 22:06:49 -0300

    Trabalhando com cotrollers

--- src/cat/cat.controller.ts
+++ src/cat/cat.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Redirect, Req } from '@nestjs/common';
+import { Request } from 'express';
+const Cats = [{id:1, nome:"Garfield"}, {id:2, nome:"Mingau"}]
 
 @Controller('cat')
 export class CatController {
-    @Get('all')
-    findAll(): string {
+    @Get(':id')
+    findOne(@Param() params: any): string {
+        const found = Cats.find((cat) => cat.id == params.id)
+        if(found == undefined){
+            return 'Cat not found'
+        }else {        
+            return `Here is the cat ${found.nome}`
+        }
+    }
+
+    @Post()
+    create(): string {
+        return 'A new cat was created';
+    }
+
+    @Get('ab*cd')
+    useWildcard() {
+        return 'This route uses a wildcard';
+    }
+
+    @Get()
+    findAll(@Req() request: Request): string {
         return 'All cats'
     }
 }
